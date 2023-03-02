472fc3743fd200c9f3702485f0754b7130db1e2b
Santanu Jana <sanujana7551@gmail.com>
2023-03-03 00:15:59 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      hi
-    </>
-  );
+    <div>
+    <Navbar/>
+    </div>
+  )
 }
 
 export default App;
