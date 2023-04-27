2c89d7a099b84f2a16818ce9fb99dd09f6a323e4
Sourav Bain <souravbain8@gmail.com>
2023-04-27 13:42:05 +0530

    prfile fixing

--- src/components/Api/StoreToken/StoreToken.js
+++ src/components/Api/StoreToken/StoreToken.js
@@ -7,9 +7,14 @@ const Gettoken=()=>{
    let token= localStorage.getItem('access_token');
    return token;
 }
+const Get_Refresh_token=()=>{
+    let token= localStorage.getItem('refresh_token');
+    return token;
+ }
+ 
 
 const Removetoken=(token)=>{
 localStorage.removeItem(token);
 }
 
-export {Storetoken,Gettoken,Removetoken};
+export {Storetoken,Gettoken,Removetoken,Get_Refresh_token};
---
+++ src/components/signup/ForgetPassword/ChangePasssword.js
@@ -0,0 +1,104 @@
+
+import { Card,Alert,Button,CardContent, Grid, TextField, Box} from '@mui/material'
+import { useState } from 'react';
+import React from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ChangePassword } from '../../Api/axios';
+import { Gettoken } from '../../Api/StoreToken/StoreToken';
+
+
+export default function ChangePass() {
+
+    const theme = createTheme({
+        palette: {
+          primary: {
+            main: '#FFA500'
+          }
+        },
+      });
+
+      
+      // const navigate=useNavigate();
+      const[error,seterror]=useState({
+        status:false,
+        msg:"",
+        type:""
+
+      })
+      const handlesubmit=async(e)=>{
+        e.preventDefault();
+        const data=new FormData(e.currentTarget);
+        const actualData={
+          
+           
+            password:data.get('password'),
+            password_confirmation:data.get('Confirm_Password'),
+          
+        }
+        const token=Gettoken();
+        if (actualData.password && actualData.password_confirmation) {
+         if (actualData.password===actualData.password_confirmation) {
+          if (actualData.password.length<8) {
+            seterror({status:true,msg:'Password must be 8 character',type:'error'})
+          }
+          else{
+            const res=await ChangePassword(actualData,token);
+            console.log(res.data);
+            if (res.data.status==='success') {
+              
+              seterror({status:true,msg:res.data.message,type:'success'})
+              document.getElementById('Reset-form').reset();
+              
+            }
+            if (res.data.status==='failed') {
+              seterror({status:true,msg:res.data.message,type:'error'})
+              document.getElementById('Reset-form').reset();
+            }
+                 
+          }
+         }
+         else{
+          seterror({status:true,msg:'Confirm Password does not match',type:'error'})
+         }
+        }
+        else{
+          seterror({status:true,msg:'All Fields are Required',type:'error'})
+        }
+
+      }
+  return (
+    <div>
+    <ThemeProvider theme={theme}>
+    <Box component='form' id='Reset-form' onSubmit={handlesubmit}>
+     <Card className='sresponsive' style={{ width: '90%', margin: "5% auto" }}>
+       <CardContent style={{ textAlign: 'center' }}>
+       <h2  style={{ color: "#FFA500"  }}>Change Password</h2>
+           <Grid container spacing={2}>
+           
+
+          
+
+           <Grid item xs={12}>
+           <TextField color="primary" type='password' label='Password*' name='password' variant="outlined" fullWidth   />
+           </Grid>
+
+           <Grid item xs={12}>
+           <TextField color="primary" type='password' label='Confirm Password*' name='Confirm_Password' variant="outlined" fullWidth   />
+           </Grid>
+           
+           <Grid item xs={12} >
+           <Button style={{width:'40%'}} type='submit' variant='contained' color='primary' sx={{color:'white'}}  >Reset</Button>
+           </Grid>
+
+           <Grid item xs={12}>
+           {error.status?<Alert severity={error.type}>{error.msg}</Alert>:''}
+           </Grid>
+
+           </Grid>
+       </CardContent>
+     </Card>
+     </Box>
+     </ThemeProvider>
+   </div>
+  )
+}
