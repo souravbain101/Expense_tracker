a052c48ea8a59c3c21b46bd0f64710e93c1f06d6
Santanu <sanujana7551@gmail.com>
2023-08-21 16:55:09 +0530

    hi3

--- src/components/showfiles/Table.js
+++ src/components/showfiles/Table.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useMemo, useState, useRef } from "react";
-import Button from "@mui/material/Button";
 import MaterialReactTable from "material-react-table";
 import { FetchAllExpences } from "../Api/axios";
 import { Gettoken } from "../Api/StoreToken/StoreToken";
 import { useReactToPrint } from "react-to-print";
 import "./Table.css";
+import { Delete, Edit } from "@mui/icons-material";
+import { Box, Button, IconButton, Tooltip } from "@mui/material";
 
 //nested data is ok, see accessorKeys in ColumnDef below
 const Example = () => {
@@ -16,15 +17,23 @@ const Example = () => {
     // console.log(res.data);
   };
 
+  const handleSaveRowEdits = () => {
+    console.log("handleSaveRowEdits");
+  };
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const handleCancelRowEdits = () => {
+    console.log("handleCancelRowEdits");
+    setCreateModalOpen(false);
+  };
+  const handleDeleteRow = () => {
+    console.log("handleDeleteRow");
+    setCreateModalOpen(false);
+  };
   // printing for pdf
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
-  const handleSaveRow =({exitEditingMode})=>{
-    console.log("gg")
-    exitEditingMode();
-  }
 
   useEffect(() => {
     GelAllDatas();
@@ -56,14 +65,35 @@ const Example = () => {
   return (
     <>
       <div ref={componentRef}>
-        <MaterialReactTable columns={columns} data={mydata} enableEditing onEditingRowSave={handleSaveRow} />
+        <MaterialReactTable
+          columns={columns}
+          data={mydata}
+          editingMode="modal" //default
+          enableColumnOrdering
+          enableEditing
+          onEditingRowSave={handleSaveRowEdits}
+          onEditingRowCancel={handleCancelRowEdits}
+          renderRowActions={({ row, table }) => (
+            <Box sx={{ display: "flex", gap: "1rem" }}>
+              <Tooltip arrow placement="left" title="Edit">
+                <IconButton onClick={() => table.setEditingRow(row)}>
+                  <Edit />
+                </IconButton>
+              </Tooltip>
+              <Tooltip arrow placement="right" title="Delete">
+                <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                  <Delete />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+        />
       </div>
       <div className="align-right">
         <Button sx={{ bgcolor: "orange", mt: 1.5, ":hover": { bgcolor: "#AF5", color: "black" } }} variant="contained" color="success" onClick={handlePrint}>
           Download
         </Button>
       </div>
-      {/* <button onClick={handlePrint}>Print this out!</button> */}
     </>
   );
 };
